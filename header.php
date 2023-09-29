d69201c6b405967d41d8f4ce117e0cbf25fc34d8
Samson Zhang <wwsamson@yahoo.com>
2023-09-29 02:26:09 -0700

    wip header

--- header.php
+++ header.php
@@ -11,4 +11,25 @@ wp_head();
 ?>
 </head>
 <body>
-    <div>header</div>
+    <div class="w-full sticky top-0 left-0 border-t-[10px] border-t-tred h-[72px] bg-white border-b z-20">
+        <div class="absolute top-0 w-full h-full flex items-center justify-center">
+            <a href="<?php echo home_url("/"); ?>" class="inline-block -mt-2 h-9 relative z-40">
+                <img src="<?php echo get_template_directory_uri() . "/img/header-logo.svg" ?>" alt="" class="h-full block">
+            </a>
+        </div>
+        <div class="max-w-7xl px-4 mx-auto flex items-center h-full relative z-30">
+            <p class="font-garamond font-medium">Documenting and amplifying 5C organizing</p>
+            <div class="flex items-center h-full ml-auto">
+                <a href="" class="ml-8 opacity-50 font-medium text-sm">About Us</a>
+                <a href="" class="ml-8 opacity-50 font-medium text-sm">Editorial standards</a>
+                <a href="" class="ml-8 rounded font-bold tracking-[2px] text-white text-xs px-4 py-3 bg-tred uppercase">Join Us</a>
+            </div>
+        </div>
+    </div>
+    <div class="w-full flex items-center justify-center h-12">
+        <a href="" class="mx-4 opacity-50 text-sm">Labor</a>
+        <a href="" class="mx-4 opacity-50 text-sm">Palestine</a>
+        <a href="" class="mx-4 opacity-50 text-sm">Abolition</a>
+        <a href="" class="mx-4 opacity-50 text-sm">Commentary</a>
+        <a href="" class="mx-4 opacity-50 text-sm">Print</a>
+    </div>
