f43de3b0aa2fa6fac099734252b8457edf176484
Samson Zhang <wwsamson@yahoo.com>
2023-09-30 02:43:38 -0700

    make post preview mobile responsive

--- template_parts/post.php
+++ template_parts/post.php
@@ -1,6 +1,6 @@
-<a class="flex mb-16" href="<?php the_permalink() ?>">
-    <?php echo get_the_post_thumbnail(null, "full", array("class" => "w-60 h-48 object-cover flex-shrink-0")) ?>
-    <div class="ml-8">
+<a class="flex mb-8 sm:mb-16" href="<?php the_permalink() ?>">
+    <?php echo get_the_post_thumbnail(null, "full", array("class" => "w-28 h-28 h- sm:w-60 sm:h-48 object-cover flex-shrink-0")) ?>
+    <div class="ml-6 sm:ml-8">
         <div class="flex items-center mb-4 text-xs">
             <?php
             $tags = get_the_tags();
@@ -18,10 +18,10 @@
                 <?php echo the_date("F j, Y"); ?>
             </p>
         </div>
-        <h3 class="font-garamond text-2xl mb-4 leading-[28px]">
+        <h3 class="font-garamond text-xl sm:text-2xl mb-4 leading-tight sm:leading-[28px]">
             <?php the_title(); ?>
         </h3>
-        <p class="text-sm opacity-50">
+        <p class="sm:text-sm opacity-50 hidden sm:block">
             <?php echo get_the_excerpt(); ?>
         </p>
     </div>
