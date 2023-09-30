8673fc62b0d3e1388cc8510d7ada613048b26a3c
Samson Zhang <wwsamson@yahoo.com>
2023-09-30 03:42:34 -0700

    add footer

--- footer.php
+++ footer.php
@@ -1,2 +1,42 @@
-<div>footer</div>
+<div class="w-full h-8 bg-black uppercase font-medium tracking-[1px] text-xs text-white flex items-center justify-center">
+    <span>Thanks for reading Undercurrents</span>
+</div>
+<div class="w-full bg-gradient-radial from-[#220000] to-tdark pb-16 lg:pb-0 relative">
+    <div class="absolute right-0 top-0 h-full bg-[#222222] w-[calc(50%-500px)] hidden lg:block"></div>
+    <div class="max-w-4xl lg:max-w-7xl px-4 mx-auto lg:flex relative">
+        <div class="lg:w-[60%] pr-4 py-16 lg:py-32 text-center lg:text-left">
+            <img src="<?php echo get_template_directory_uri() . "/img/square-logo.png" ?>" alt="" class="w-16 mb-8 block rounded mx-auto lg:mx-0">
+            <p class="font-garamond font-medium text-3xl sm:text-4xl md:text-[45px] !leading-tight hero-text hero-text-light">Undercurrents reports on <a><i>labor</i></a>, <a><i>Palestine liberation</i></a>, <a><i>prison abolition</i></a>, <a><i>tenants’ rights</i></a> and other community organizing at and around the Claremont Colleges.</p>
+            <div class="flex justify-center lg:justify-start items-center mt-12">
+                <a href="" class="text-white opacity-50 hover:opacity-100 mr-8 whitespace-nowrap">About us</a>
+                <a href="" class="text-white opacity-50 hover:opacity-100 mr-8 whitespace-nowrap">Editorial standards</a>
+                <a href="" class="text-white opacity-50 hover:opacity-100 lg:mr-8 whitespace-nowrap">Join us</a>
+            </div>
+        </div>
+        <div class="lg:w-[40%] flex-shrink-0 flex items-center max-w-md mx-auto relative mt-8 lg:mt-0">
+            <div class="absolute top-0 left-[40%] w-3/5 bg-[#222222] h-full" id="issue-promo-back-footer"></div>
+            <style>
+                #issue-promo-back-footer:before {
+                    content: "";
+                    transform: skewX(-8deg) translateX(-50%);
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 55%;
+                    height: 100%;
+                    background-color: #222;
+                }
+            </style>
+            <img src="<?php echo get_template_directory_uri() . "/img/issue1.png" ?>" alt="" class="w-2/5 sm:w-1/2 flex-shrink- pr-4 relative py-4 block">
+            <div class="w-3/5 sm:w-1/2 flex-shrink-0 text-white relative pr-4 py-8">
+                <p class="text-[10px] uppercase tracking-[1px] mb-2">
+                    <span class="font-bold">Issue 1</span> / Spring 2023
+                </p>
+                <p class="text-2xl font-garamond font-medium mb-2">Setting the Standard</p>
+                <p class="opacity-75 text-xs mb-4">How Pomona workers won a historic $25 minimum wage; a new union in Claremont; Tony Hoang on organizing</p>
+                <a href="" class="bg-tred py-2 px-3 text-xs uppercase text-white rounded tracking-[1px] font-bold">Read issue 1</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
             "tdarkred": "#D12229",
             "tdark": "#111111",
         },
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        }
       },
     },
     plugins: [],
